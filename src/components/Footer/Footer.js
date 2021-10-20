4f9ae798d46c5979437ad722f9a5ede75a856517
coder71 <kamrunnaher877@gmail.com>
2021-10-20 21:53:09 +0600

    footer and 404 not found page added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Button, Col, FormControl, InputGroup, Row } from 'react-bootstrap';
 
 const Footer = () => {
-  return <div>this is footer</div>;
+  return (
+    <footer>
+      <Row className="align-items-center bg-primary p-3">
+        <Col className="fw-bold text-uppercase text-white">
+          Sign up for our weekly newsletter
+        </Col>
+        <Col>
+          <InputGroup>
+            <FormControl
+              className="border  border-light"
+              placeholder="Email Address"
+              aria-label="Email Address"
+              aria-describedby="basic-addon2"
+            />
+            <Button variant="outline-dark text-white" id="button-addon2">
+              Submit
+            </Button>
+          </InputGroup>
+        </Col>
+      </Row>
+      <Row className="bg-dark text-white align-items-center">
+        <Col>
+          <p className="text-uppercase fw-bold fs-1">HalthMedico</p>
+        </Col>
+        <Col>
+          <p>&copy; all rights reserved by HealthMedico</p>
+        </Col>
+      </Row>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Button, Link } from 'react-router-dom';
+import { Button, NavLink } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div
-      className=" d-flex flex-column justify-content-center align-items-center my-3 text-center"
+    <section
+      className=" d-flex flex-column justify-content-center align-items-center text-center"
       style={{ minHeight: 'calc(100vh - 200px)' }}
     >
-      <h3 className="text-danger d-1 mb-2">404</h3>
-      <p className="text-info">Sorry We couldn't find this page.</p>
-      <Link to="/home">
-        <Button variant="outline-primary">Go to Home</Button>
-      </Link>
-    </div>
+      <p style={{ fontSize: 100 }} className="text-danger">
+        404
+      </p>
+      <p className="fs-4 text-info">Sorry! we couldn't find this page</p>
+      <p>Please go back to home page</p>
+      <NavLink to="/home">
+        <Button variant="info text-white">Home</Button>
+      </NavLink>
+    </section>
   );
 };
 
